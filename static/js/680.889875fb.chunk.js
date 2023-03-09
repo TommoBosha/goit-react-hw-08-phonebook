c9b2fc5c797e8b4152742b47f151418b81604a7e
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-03-09 15:27:31 +0000

    Deploying to gh-pages from @ TommoBosha/goit-react-hw-08-phonebook@6797280f84411223228ddf2d7256b37cd136112e 🚀

---
+++ static/js/680.889875fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,o,n){n.r(o);var t=n(184);o.default=function(){return(0,t.jsx)("h1",{children:"Welcome to Phonebook App"})}}}]);
+//# sourceMappingURL=680.889875fb.chunk.js.map
